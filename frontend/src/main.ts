13e080b60146d2a491ba873a5a23637918c61e50
Patryk Dowgiert <patryk.dowgiert@infotech.edu.pl>
2025-11-13 12:21:44 +0100

    fsd

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-<<<<<<< HEAD
-// Poprawiamy import z 'App' na 'AppComponent'
-import { AppComponent } from './app/app'; 
-
-// Poprawiamy start aplikacji, aby używała 'AppComponent'
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
 import { AppComponent } from './app/app';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
->>>>>>> 58522b42a267c379e15619ed019298ad97af582d
