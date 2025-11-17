a36308bc64aeff91d65729b7819b568194396728
Lewosz Jakub <lewoj@it.local>
2025-11-17 11:01:30 +0100

    cos

--- backend/DTOs/MessageDto.cs
+++ backend/DTOs/MessageDto.cs
@@ -9,6 +9,12 @@ namespace backend.DTOs
         public string Tresc { get; set; } = string.Empty;
         public DateTime DataWyslania { get; set; }
         public AuthorDto Autor { get; set; } = null!;
+        
+        // --- NOWE POLE ---
+        // True, jeśli wiadomość pochodzi od pracownika UKNF (Admina lub Merytorycznego)
+        public bool IsAdmin { get; set; } 
+        // -----------------
+
         public List<AttachmentDto> Zalaczniki { get; set; } = new List<AttachmentDto>();
     }
 }
